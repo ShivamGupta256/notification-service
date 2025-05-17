7a517450622e9145495b607333403e09eb41d059
Shivam-Gupta256 <shiva256018@gmail.com>
2025-05-17 18:16:39 +0530

    Connect to DB

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app =express();
 const dotenv = require("dotenv");
-
 dotenv.config();
+const connectDB = require("./config/db");
+connectDB();
 
 app.use(express.json());
 
