57fdcf98462a15d0715cb47996467f9f5161ea76
Shivam-Gupta256 <shiva256018@gmail.com>
2025-05-17 18:29:40 +0530

    Create notifiaction schema

---
+++ src/models/Notification.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const notificationSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        reuired: true
+    },
+    type: {
+        type:String,
+        enum: ["email"," sms", "in-app"],
+            required: true
+    },
+    to: {
+        type: String
+    },
+    subject: {
+        type: String
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["pending", "sent", "failed"],
+        default: "pending"
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("Notification", notificationSchema);
