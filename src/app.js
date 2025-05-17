4246955e52c25499994218a3d471b676d7575eb1
Shivam-Gupta256 <shiva256018@gmail.com>
2025-05-17 17:25:40 +0530

    Add express server

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app =express();
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+app.use(express.json());
+
+app.get("/", (req,res) => {
+    res.send("Notification service API is running");
+});
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, ()=> {
+    console.log(`server live on port ${PORT}`);
+});
