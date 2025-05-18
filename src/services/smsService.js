44cdff0ee6880dc6b6a400b01c3a53972c8f7fbe
Shivam-Gupta256 <shiva256018@gmail.com>
2025-05-18 13:06:07 +0530

    Integrated real sms sending using twilio free trial account

--- src/services/smsService.js
+++ src/services/smsService.js
@@ -1,6 +1,26 @@
-const sendSMS = async (notification) => {
+/*const sendSMS = async (notification) => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
   console.log(`[Mock] SMS sent to ${notification.to}: ${notification.message}`);
+};
+
+module.exports = sendSMS;
+*/
+
+const twilio = require('twilio');
+
+const client = twilio(
+  process.env.TWILIO_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
+
+const sendSMS = async (notification) => {
+  await client.messages.create({
+    body: notification.message,
+    from: process.env.TWILIO_PHONE,
+    to: notification.to,
+  });
+
+  console.log(`SMS sent to ${notification.to}`);
 };
 
 module.exports = sendSMS;
